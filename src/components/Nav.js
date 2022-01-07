4199aee226d3411a090d143c5223dc5299e6dc92
Langrangee <EmmanuelN1>
2022-01-07 20:48:49 +0100

    push 1.0

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+
+function Nav() {
+    return (
+        <div>
+            <nav className="container flex items-center mt-4 py-4 sm:mt-12">
+                
+            </nav>
+        </div>
+    )
+}
+
+export default Nav;
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Nav from "../components/Nav"
+
+export default function Home() {
+  return (
+    <div>
+        <Head>
+          <title>bOffice</title>
+        </Head>
+        <Nav/>
+      
+    </div>
+  );
+}
